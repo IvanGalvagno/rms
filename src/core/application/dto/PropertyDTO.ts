b24593996f8169e93679e866db641451b86551fb
Ivan Galvagno <ivan.galvagno@gmail.com>
2025-05-31 19:19:04 -0300

    property dto

---
+++ src/core/application/dto/PropertyDTO.ts
@@ -0,0 +1,36 @@
+import { PropertyType } from "@/core/domain/entities/Property";
+
+/**
+ * DTO para a criação de um novo imóvel.
+ * Contém os dados necessários que vêm da camada de apresentação (ou API).
+ */
+export interface CreatePropertyDTO {
+    address: string;
+    type: PropertyType; // Usando o enum definido na entidade para consistência
+    description?: string;
+    bedrooms: number;
+    bathrooms: number;
+    area: number;
+    rentAmount: number;
+    parkingSpaces?: number; // número de vagas de garagem
+}
+
+/**
+ * DTO para representar os detalhes de um imóvel.
+ * Usado como um objeto de retorno padronizado pelos casos de uso.
+ * Pode ser uma versão simplificada ou formatada da entidade Property.
+ */
+export interface PropertyDetailsDTO {
+  id: string;
+  address: string;
+  type: PropertyType;
+  description?: string;
+  bedrooms: number;
+  bathrooms: number;
+  area: number;
+  rentAmount: number;
+  // ownerId?: string;
+  // status?: string;
+  createdAt: string; // Datas podem ser strings ISO para facilitar o trânsito (ex: JSON)
+  updatedAt: string;
+}
